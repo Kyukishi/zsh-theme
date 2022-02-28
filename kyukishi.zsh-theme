6fe573db529b0cd25f2dc366f218e8a13c1992ec
Kyukishi <52956331+Kyukishi@users.noreply.github.com>
2022-02-28 22:19:37 +0100

    Initial commit

---
+++ kyukishi.zsh-theme
@@ -0,0 +1,75 @@
+# Configuration
+
+# Root Directory
+function get_root() {
+  echo "$ZSH/custom/themes/kyukishi"
+}
+
+# Icons
+builtin source $(get_root)/src/icons.zsh
+
+function get_config() {
+# If you don't know what these values mean you should leave them as they are!
+
+  # Left Prompt
+  local prompt_left='<username><path><git>'
+  local prompt_left_background='$BG[237]'
+  local prompt_left_seperator='  '
+  local prompt_left_seperator_color='$FG[245]'
+  local prompt_left_before=" $(get_icon APPLE_ICON) "
+  # local prompt_left_before_color=
+  local prompt_left_after=''
+  local prompt_left_after_color='%b$FG[235]'
+
+  # Right Prompt
+  local prompt_right='<success>'
+
+  # Username
+  local username_user_color="$FG[169]"
+  local username_host_show=false
+  # local username_host_color=
+  # local username_seperator='@'
+  # local username_seperator_color=
+  # local username_before=
+  # local username_before_color=
+  local username_after=''
+  local username_after_color=' $BG[235]$FG[237]'
+
+  # Path
+  local path_color='$BG[235]$FG[117]'
+  # local path_icon_home= # unimplemented
+  # local path_icon_root= # unimplemented
+  local path_icon_default="$(get_icon FOLDER_ICON) "
+  local path_before=' '
+  # local path_before_color=
+  local path_after=' '
+  # local path_after_color=
+
+  # Git
+  # local git_color=
+  local git_prefix="$(get_icon VCS_GIT_ICON)("
+  local git_suffix=')'
+  local git_dirty='*'
+  local git_clean=''
+  local git_before=" "
+  local git_before_color='$FG[237]'
+  local git_after=' '
+  # local git_after_color=
+
+  # Multiline
+  local multiline=true
+  # local multiline_first_prefix=
+  # local multiline_first_prefix_color=
+  local multiline_last_prefix=" $(get_icon MULTILINE_LAST_PROMPT_PREFIX)"
+  # local multiline_last_prefix_color=
+
+  # Prefix
+  local prefix='$ '
+  # local prefix_color=
+
+  eval "local value=\"\${$1}\""
+  echo "$value"
+}
+
+# Load the main script
+builtin source $(get_root)/src/kyukishi.zsh
---
+++ src/kyukishi.zsh
@@ -0,0 +1,109 @@
+function get_username() {
+  if [ $(get_config username_host_show) = true ]
+  then
+    local hostname="\
+%{$(get_config username_seperator_color)%}$(get_config username_seperator)%{%f%}\
+%{$(get_config username_host_color)%}$HOST%{%f%}\
+"
+  fi
+
+  echo "\
+%{$(get_config username_before_color)%}$(get_config username_before)%{%f%}\
+%{$(get_config username_user_color)%}$USER%{%f%}$hostname\
+%{$(get_config username_after_color)%}$(get_config username_after)%{%f%}\
+"
+}
+
+function get_path() {
+  local icon="$(get_config path_icon_default)"
+
+  echo "\
+%{$(get_config path_before_color)%}$(get_config path_before)%{%f%}\
+%{$(get_config path_color)%}$icon%~%{%f%}\
+%{$(get_config path_after_color)%}$(get_config path_after)%{%f%}\
+"
+}
+
+function get_success() {
+  echo '%(?.✔.%{$fg[red]%}✘%f)'
+}
+
+function get_git() {
+  ZSH_THEME_GIT_PROMPT_PREFIX="$(get_config git_prefix)"
+  ZSH_THEME_GIT_PROMPT_SUFFIX="$(get_config git_suffix)"
+  ZSH_THEME_GIT_PROMPT_DIRTY="$(get_config git_dirty)"
+  ZSH_THEME_GIT_PROMPT_CLEAN="$(get_config git_clean)"
+  echo "\
+%{$(get_config git_before_color)%}$(get_config git_before)%{%f%}\
+%{$(get_config git_color)%}$(git_prompt_info)%{%f%}\
+%{$(get_config git_after_color)%}$(get_config git_after)%{%f%}\
+"
+}
+
+function get_prefix() {
+  if [ $(get_config multiline) = true ]
+  then
+    local multiline_last_prefix="%{$(get_config multiline_last_prefix_color)%}$(get_config multiline_last_prefix)%{%f%}"
+  fi
+
+  echo "$multiline_last_prefix%{$(get_config prefix_color)%}$(get_config prefix)%{%f%}"
+}
+
+function get_seperator_left() {
+  echo "%{$(get_config prompt_left_seperator_color)%}$(get_config prompt_left_seperator)%{$(get_config prompt_left_background)%}%{%f%}"
+}
+
+function get_prompt() {
+  local value="${@:1}"
+
+  value="${value//<username>/$(get_username)}"
+  value="${value//<sep>/$(get_seperator_left)}"
+  value="${value//<seperator>/$(get_seperator_left)}"
+  value="${value//<path>/$(get_path)}"
+  value="${value//<success>/$(get_success)}"
+  value="${value//<git>/$(get_git)}"
+
+  echo "$value"
+}
+
+function get_prompt_left() {
+  if [ $(get_config multiline) = true ]
+  then
+    local multiline_first_prefix="%{$(get_config multiline_first_prefix_color)%}$(get_config multiline_first_prefix)%{%f%}"
+    local multiline_break='\n'
+  fi
+
+  echo "\
+$multiline_first_prefix\
+%{$(get_config prompt_left_background)%}\
+%{$(get_config prompt_left_before_color)%}$(get_config prompt_left_before)%{%f%}\
+%{$(get_config prompt_left_background)%}\
+$(get_prompt $(get_config prompt_left))\
+%{$(get_config prompt_left_background)%}\
+%{$(get_config prompt_left_after_color)%}$(get_config prompt_left_after)%{%f%}\
+%{%b%}\
+$multiline_break$(get_prefix)"
+}
+
+function get_prompt_right() {
+  echo "$(get_prompt $(get_config prompt_right))"
+}
+
+PROMPT="$(get_prompt_left)"
+
+RPROMPT="$(get_prompt_right)"
+
+# Cleanup
+unfunction get_root
+unfunction get_config
+unfunction get_icon
+
+unfunction get_username
+unfunction get_prefix
+unfunction get_prompt
+unfunction get_prompt_left
+unfunction get_prompt_right
+unfunction get_path
+unfunction get_success
+unfunction get_git
+unfunction get_seperator_left
